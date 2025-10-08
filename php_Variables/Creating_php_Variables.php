137dee2667bf3481e2d42255faecca188291f38b
¢яιмσяуи <selcantaylan@gmail.com>
2025-10-08 20:37:53 +0300

    Add files via upload

---
+++ php_Variables/Creating_php_Variables.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+$y = "Hunter Doohan";
+$s= 10;
+
+echo $x+$s;
+echo "<br>";
+echo $y;
+echo "<br>";
+// var_dump:To get the data type of a variable
+var_dump(5);
+echo "<br>";
+var_dump("Hunter Doohan");
+echo "<br>";
+var_dump(3.14);
+echo "<br>";
+var_dump(true);
+echo "<br>";
+var_dump([2, 3, 56]);
+echo "<br>";
+var_dump(NULL);
+
+?>
+
+</body>
+</html>
