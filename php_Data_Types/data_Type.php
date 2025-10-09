40c22b20eed956794e1d8e81bf2dacf39935214b
¢яιмσяуи <selcantaylan@gmail.com>
2025-10-09 22:09:53 +0300

    Add files via upload

---
+++ php_Data_Types/data_Type.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+//string data type
+$x = "Hello world!";
+$y = 'Hello world!';
+var_dump($x);
+echo "<br>"; 
+var_dump($y);
+echo "<br>"; 
+//integers data type
+$i = 5985;
+var_dump($i);
+echo "<br>"; 
+//float data type
+$f = 10.365;
+var_dump($f);
+echo "<br>"; 
+//bool data type 
+$b = true;
+var_dump($b);
+echo "<br>"; 
+//array data type
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+?>
+
+</body>
+</html>
